f92225575e51fce3819abd76af606c76e081ff5a
华正超 <huazhengchao@vdongchina.com>
2020-12-28 12:50:55 +0800

    获取清单接口

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/huazhengchao/wanzi_bubble
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/gorm v1.20.9
+)
--- main.go
+++ main.go
@@ -1,19 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"net/http"
 )
 
+var (
+	DB *gorm.DB
+)
+
+type Todo struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Status int `json:"status"`
+}
+
+func initMysql() (err error) {
+	dsn := "root:123456@(127.0.0.1:3305)/bubble"
+	DB, err = gorm.Open(mysql.Open(dsn), nil)
+	return
+}
+
 func main() {
+
+	err := initMysql()
+	if err != nil {
+		panic(err)
+	}
 
 	r := gin.Default()
 
+	// 配置模板及静态资源目录
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/static", "static")
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
+
+	V1Group := r.Group("/v1")
+	{
+		V1Group.GET("/todo", func(c *gin.Context) {
+			var todo []Todo
+			DB.Find(&todo)
+			c.JSON(http.StatusOK, todo)
+		})
+		
+
+	}
 
 	r.Run(":85")
 }
